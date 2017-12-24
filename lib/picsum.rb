2fa0bde47d4973863e0cf07a429c00f2c90768e1
geekloper <derfoufiabdel@gmail.com>
2017-12-24 03:42:40 +0100

    update

--- lib/picsum.rb
+++ lib/picsum.rb
@@ -6,19 +6,28 @@ module Picsum
   
   class Base
     
-    def self.generate ( number_pictures = 5 , width = 200 , height = 300)
+    def self.generate ( number_pictures = 5 , width = 500 , height = 500)
       
       for i in 0...number_pictures
 
-        open( 'tmp/' + SecureRandom.urlsafe_base64(5) + '.jpeg', 'wb') do |file|
-          file << open('https://picsum.photos/#{width}/#{height}/?random').read
-        end
+        self.save_img width , height
 
         print (number_pictures - 1 - i).to_s + " remaining picture(s)...\r"
         $stdout.flush
       
       end   
     end
+
+    private
+
+    def self.save_img width , height
+
+      open( SecureRandom.urlsafe_base64(5) + '.jpeg', 'wb') do |file|
+        file << open("https://picsum.photos/#{width}/#{height}/?random").read
+      end
+
+    end
+
 
   end
 
