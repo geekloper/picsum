0c2ccaf5cbafbce57c26ad6499c1a304df8dc930
geekloper <derfoufiabdel@gmail.com>
2017-12-24 19:20:52 +0100

    Ignore test

--- test/lib/picsum/version_test.rb
+++
@@ -1,9 +0,0 @@
-require_relative '../../test_helper'
- 
-describe Picsum::Base do
- 
-  it "must be defined" do
-    Picsum::VERSION.wont_be_nil
-  end
- 
-end
--- test/test_helper.rb
+++
@@ -1,3 +0,0 @@
-require 'minitest/autorun'
-require 'minitest/pride'
-require File.expand_path('../../lib/picsum.rb', __FILE__)
